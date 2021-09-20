e303aaad2befa020e2df05edc0003c1839e357b0
asadurjaman <assadurjamansagor24@gmail.com>
2021-09-20 14:55:32 +0600

    home page added

--- resources/views/Website/header.blade.php
+++ resources/views/Website/header.blade.php
@@ -1,9 +1,9 @@
 <section class="quiz-headline" id="myDiv">
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-6">
               <img src="{{asset('img\main-logo.webp')}}" class="img-fluid" alt="main-logo">
-                <h1>Delicious Ultimate Quiz </h1>
+              <h1>Delicious Ultimate Quiz </h1>
             </div>
         </div>
     </div>
