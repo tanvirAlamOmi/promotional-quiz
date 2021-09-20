e996f50686f001561549298289036206c3d22bd6
asadurjaman <assadurjamansagor24@gmail.com>
2021-09-20 15:54:28 +0600

    home page new

--- resources/views/Website/home.blade.php
+++ resources/views/Website/home.blade.php
@@ -1,5 +1,5 @@
 
-    <section class="home-page">
+    <!-- <section class="home-page">
         <div class="container">
           <div class="row">
             <div class="col-12">
@@ -13,11 +13,32 @@
         <div class="container-fluid" style="padding: 0px;">
         <img src="{{asset('img\sand-two.webp')}}" class="sand-img" alt="">
         </div>
-        <div class="container" style="padding: 6% 0% 3% 0%;">
+        <div class="container">
           <div class="row align-items-end">
           <div class="col-12">
             <h1>Win a free sandwich</h1>
             <button class="home-btn" id="go_to_quiz">Take Quiz</button>
+            </div>
+          </div>
+        </div>
+      </section> -->
+      <section class="home-page">
+        <div class="container">
+          <div class="row">
+            <div class="card">
+            <div class="col-12">
+             <img src="{{asset('img\main-logo.webp')}}" class="logo-home" alt="main-logo">
+            </div>
+            <div class="col-12">
+              <img src="{{asset('img\soul-sand.webp')}}" class="soul-sand" alt="">
+            </div>
+            <div class="col-12">
+             <img src="{{asset('img\sand-two.webp')}}" class="sand-img" alt="">
+            </div>
+            <div class="col-12">
+            <h1>Win a free sandwich</h1>
+            <button class="home-btn" id="go_to_quiz">Take Quiz</button>
+            </div>
             </div>
           </div>
         </div>
